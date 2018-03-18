1dece0135b2fd21b08cecb64b95389825f7f71ca
Ryan Reed <reedr@usengineering.com>
2018-03-18 18:49:36 -0500

    Add Static Routes

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var path = require('path');
 var api = require('./api/index');
 
 var app = express();
-
 
 ///////////////////////
 // Server Middleware
 ///////////////////////
 
-app.use(logger(app.get("env") === "production" ? "combined" : "dev"));
+app.use(logger(app.get('env') === 'production' ? 'combined' : 'dev'));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -22,33 +21,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // CORS
 // This allows client applications from other domains use the API Server
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
-
 
 //////////////////
 // API Queries
 //////////////////
+app.use('/static', express.static(path.resolve(__dirname, './static')));
+app.get('/', (req, res) => res.redirect('/static/'));
 
 app.use('/', api);
-
 
 //////////////////
 // Server Setup
 //////////////////
 
-app.set("env", process.env.NODE_ENV || "development");
-app.set("host", process.env.HOST || "0.0.0.0");
-app.set("port", process.env.PORT || 3000);
+app.set('env', process.env.NODE_ENV || 'development');
+app.set('host', process.env.HOST || '0.0.0.0');
+app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get("port"), function () {
-    console.log('\n' + '**********************************');
-    console.log('REST API listening on port ' + app.get("port"));
-    console.log('**********************************' + '\n');
+app.listen(app.get('port'), function() {
+  console.log('\n' + '**********************************');
+  console.log('REST API listening on port ' + app.get('port'));
+  console.log('**********************************' + '\n');
 });
-
 
 ////////////////////
 // Error Handlers
@@ -56,32 +54,29 @@ app.listen(app.get("port"), function () {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status( err.code || 500 )
-        .json({
-            status: 'error',
-            message: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.code || 500).json({
+      status: 'error',
+      message: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
-    .json({
-        status: 'error',
-        message: err.message
-    });
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message
+  });
 });
-
 
 module.exports = app;
